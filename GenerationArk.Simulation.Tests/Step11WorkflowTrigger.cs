524187775458e70ef769b05fe5f12abf21dcb970
Merverli <calvinbrady8@gmail.com>
2026-07-19 13:32:03 -0500

    Trigger Step 11 validation after soak registration ref correction

---
+++ GenerationArk.Simulation.Tests/Step11WorkflowTrigger.cs
@@ -0,0 +1,7 @@
+namespace GenerationArk.Simulation.Tests;
+
+// Behavior-neutral source marker used to emit a pull-request synchronize event
+// after the Step 11 soak registration branch ref was corrected.
+internal static class Step11WorkflowTrigger
+{
+}
